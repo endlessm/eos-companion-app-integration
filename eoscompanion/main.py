040d36d2a49b74f254f39ae7568101459e2bac4d
Sam Spilsbury <sam@endlessm.com>
2017-11-18 11:18:04 +0800

    eoscompanion: Boostrap simple python GApplication

---
+++ eoscompanion/main.py
@@ -0,0 +1,59 @@
+# /eoscompanion/main.py
+#
+# Copyright (C) 2017 Endless Mobile, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# All rights reserved.
+'''Main executable entry point for eos-companion-app-service.'''
+
+import sys
+
+from gi.repository import GLib, Gio
+
+
+class CompanionAppApplication(Gio.Application):
+    '''Subclass of GApplication for controlling the companion app.'''
+
+    def __init__(self, *args, **kwargs):
+        '''Initialize the application class.'''
+        kwargs.update({
+            'application_id': 'com.endlessm.CompanionAppService',
+            'flags': Gio.Application.IS_SERVICE
+        })
+        super(CompanionAppApplication, self).__init__(*args, **kwargs)
+
+    def do_startup(self):
+        '''Just print a message.'''
+        Gio.Application.do_startup(self)
+
+    def do_dbus_register(self, connection, path):
+        '''Invoked when we get a D-Bus connection.'''
+        print('Got d-bus connection at {path}'.format(path=path))
+        return Gio.Application.do_dbus_register(self, connection, path)
+
+    def do_dbus_unregister(self, connection, path):
+        '''Invoked when we lose a D-Bus connection.'''
+        print('Lost d-bus connection at {path}'.format(path=path))
+        return Gio.Application.do_dbus_unregister(self, connection, path)
+
+    def do_activate(self):
+        '''Invoked when the application is activated.'''
+        print('Activated')
+
+
+def main(args=None):
+    '''Entry point function.'''
+    CompanionAppApplication().run(args or sys.argv)
+
---
+++ setup.py
@@ -0,0 +1,52 @@
+# /setup.py
+#
+# Copyright (C) 2017 Endless Mobile, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# All rights reserved.
+'''Installation and setup script for eoscompanion.'''
+
+from setuptools import find_packages, setup
+
+setup(name='eoscompanion',
+      version='0.0.0',
+      description='''OS Integration for Endless Companion App.''',
+      long_description=('''
+        OS Integration and Avahi services for Endless OS
+        companion app.
+      '''),
+      author='Sam Spilsbury',
+      author_email='sam@endlessm.com',
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.1',
+                   'Programming Language :: Python :: 3.2',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   'Intended Audience :: Developers',
+                   'Topic :: System :: Shells',
+                   'Topic :: Utilities'],
+      url='http://github.com/endlessm/eos-companion-app-integration',
+      license='MIT',
+      keywords='development',
+      packages=find_packages(exclude=['test']),
+      install_requires=['setuptools'],
+      entry_points={
+          'console_scripts': [
+              'eos-companion-app-service=eoscompanion.main:main',
+          ]
+      },
+      zip_safe=True,
+      include_package_data=True)
