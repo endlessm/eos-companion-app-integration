3fe389eec961ab708afc7c7e3c79022d4215f738
Sam Spilsbury <sam@endlessm.com>
2017-12-08 01:26:06 +0800

    eoscompanion: Add HTML content adjuster
    Which rewrites all embedded EKN urls as urls which are resolvable
    in the app itself.
    
    The implementation here is not ideal. It'd be nice if we could use
    beautifulsoup, but that requires an external python dependency.
    
    https://phabricator.endlessm.com/T20093

--- eoscompanion/main.py
+++ eoscompanion/main.py
@@ -319,6 +319,57 @@ def load_record_from_engine_async(engine, app_id, content_id, attr, callback):
     return False
 
 
+def rewrite_ekn_url(ekn_id, query):
+    '''If the URL is an EKN url, rewrite it to be server-relative.
+
+    This causes the applicationId and deviceUUID to be included in
+    the URL query-string.
+    '''
+    formatted = format_uri_with_querystring(
+        '/content_data',
+        deviceUUID=query['deviceUUID'],
+        applicationId=query['applicationId'],
+        contentId=ekn_id
+    )
+    return formatted
+
+def ekn_url_rewriter(query):
+    '''Higher order function to rewrite a URL based on query.'''
+    return lambda m: '"{}"'.format(rewrite_ekn_url(m.group('id'), query))
+
+
+_RE_EKN_URL_CAPTURE = re.compile(r'"ekn\:\/\/\/(?P<id>[a-z0-9]+)"')
+
+
+def _html_content_adjuster(content_bytes, query):
+    '''Adjust HTML content by rewriting all the embedded URLs.
+
+    There will be images, video and links in the document, parse it
+    and rewrite it so that the links all go to somewhere that can
+    be resolved by the server.
+
+    Right now the way that this is done is a total hack (regex), in future
+    we might want to depend on beautifulsoup and use that instead.
+    '''
+    return EosCompanionAppService.string_to_bytes(
+        _RE_EKN_URL_CAPTURE.sub(ekn_url_rewriter(query),
+                                EosCompanionAppService.bytes_to_string(content_bytes))
+    )
+
+
+CONTENT_TYPE_ADJUSTERS = {
+    'text/html': _html_content_adjuster
+}
+
+
+def adjust_content(content_type, content_bytes, query):
+    '''Adjust content if necessary.'''
+    if content_type in CONTENT_TYPE_ADJUSTERS:
+        return CONTENT_TYPE_ADJUSTERS[content_type](content_bytes, query)
+
+    return content_bytes
+
+
 
 @require_query_string_param('deviceUUID')
 @require_query_string_param('applicationId')
@@ -338,10 +389,12 @@ def companion_app_server_content_data_route(server, msg, path, query, *args):
         def _on_got_data_callback(data_src, data_result):
             '''Callback function that gets called when we are done.'''
             try:
-                image_bytes = EosCompanionAppService.finish_load_all_in_stream_to_bytes(data_result)
+                data_bytes = EosCompanionAppService.finish_load_all_in_stream_to_bytes(data_result)
                 EosCompanionAppService.set_soup_message_response_bytes(msg,
                                                                        content_type,
-                                                                       image_bytes)
+                                                                       adjust_content(content_type,
+                                                                                      data_bytes,
+                                                                                      query))
             except GLib.Error as error:
                 json_response(msg, {
                     'status': 'error',
